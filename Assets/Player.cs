28e589eca42d9a5e96a99cb51304ed2ebd67192c
Sanan Mammadov <sananmammadov99@gmail.com>
2017-10-26 06:25:55 -0700

    added a new feature some files changed

---
+++ Assets/Player.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+	public string currentColor;
+	public float jumpForce = 10f;
+	public Rigidbody2D circle;
+	public SpriteRenderer sr;
+	public Color blue;
+	public Color yellow;
+	public Color pink;
+	public Color purple;
+	public static int score = 0;
+	public Text scoreText;
+
+
+	void Start () {
+		setRandomColor ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0)){
+			circle.velocity = Vector2.up * jumpForce;
+		}
+
+		scoreText.text = score.ToString();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.tag == "colorChange") {
+			setRandomColor ();
+			Destroy (collision.gameObject);
+			return;
+		}
+		if (collision.tag == "scored") {
+			score++;
+			Destroy (collision.gameObject);
+			return;
+		}
+		if (collision.tag != currentColor) {
+			Debug.Log ("DIED!!!");
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+			score = 0;
+		}
+
+
+	}
+
+	void setRandomColor(){
+		int rand = Random.Range (0, 4);
+
+		switch (rand) {
+		case 0:
+			currentColor = "blue";
+			sr.color = blue;
+			break;
+		case 1: 
+			currentColor = "yellow";
+			sr.color = yellow;
+			break;
+		case 2:
+			currentColor = "pink";
+			sr.color = pink;
+			break;
+		case 3: 
+			currentColor = "purple";
+			sr.color = purple;
+			break;
+
+		}
+	}
+}
